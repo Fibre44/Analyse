32a5a9bcb1050f04d2e0c583a58e3e451332ed67
DENIS Nicolas <denisnicolas@mac-mini-de-denis.home>
2021-01-17 18:24:19 +0100

    Mise à jour 17/01/2021

--- src/Controller/BibliothequeccnController.php
+++ src/Controller/BibliothequeccnController.php
@@ -101,7 +101,7 @@ class BibliothequeccnController extends AbstractController
             $entityManager->persist($bibliothequeccn);
             $entityManager->flush();
 
-            return $this->redirectToRoute('bibliotheque_classification_create', ['idbibliothequeccn'=>$bibliothequeccn->getId()]);
+            return $this->redirectToRoute('bibliotheque_chapitre.html.twig', ['idbibliothequeccn'=>$bibliothequeccn->getId()]);
         }
 
         return $this->render('bibliothequeccn/bibliothequeccn_create.html.twig', [
